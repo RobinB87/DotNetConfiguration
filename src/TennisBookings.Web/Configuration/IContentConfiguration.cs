d0956cc81eb4f41a75b5b55afa5484b8429d69d7
Robin Bette <robinbette@hotmail.com>
2022-03-11 14:42:08 +0100

    Add unit tests dependent on options classes

--- src/TennisBookings.Web/Configuration/IContentConfiguration.cs
+++ src/TennisBookings.Web/Configuration/IContentConfiguration.cs
@@ -1,5 +1,8 @@
 ﻿namespace TennisBookings.Web.Configuration
 {
+    /// <summary>
+    /// Interface, used for forwarding options
+    /// </summary>
     public interface IContentConfiguration
     {
         bool CheckForProfanity { get; }
